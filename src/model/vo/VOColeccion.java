ca7ac8bf0cc310daaff3116277fd4bb268ea2966
Luis Eduardo Yamin Lacouture <lyamin@uniandes.edu.co>
2019-03-24 20:03:56 -0500

    Terminar

---
+++ src/model/vo/VOColeccion.java
@@ -0,0 +1,22 @@
+package model.vo;
+
+
+import java.time.LocalDateTime;
+
+public class VOColeccion {
+	int totalInf;
+	float pSinAcc;
+	float pConAcc;
+	float totalPagar;
+	
+	LocalDateTime horaInicial;
+	LocalDateTime horaFinal;
+	
+	int addressID;
+	int StreetSegId;
+	int xCoord;
+	int yCoord;
+	
+	int voCode;
+	int vDesc;
+}
